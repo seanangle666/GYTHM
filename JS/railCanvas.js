007a845fed763e07cf009f479e2e01360d952769
seanangle666 <manda201703@gmail.com>
2025-04-03 14:16:42 +0800

    Update railCanvas.js

--- JS/railCanvas.js
+++ JS/railCanvas.js
@@ -7,16 +7,17 @@ let poslane = 0, w, h, startPos, railW = 0;
 let notes = [];
 
 class Note {
-    constructor(rail, type, time) {
+    constructor(rail, type, time, detail) {
         this.rail = rail;
         this.type = type; // 0: Tap, 1: Hold, 2: Flick, 3: Crash, 4: Change
         this.time = time;
+        this.detail = detail;
     }
 
     drawNote() {
+        let t = this.time - time;
         switch (this.type) {
             case 0: // Tap Note
-                let t = this.time - time;
                 if (t > 0) {
                     ctx.lineWidth = 25 / t;
                     ctx.strokeStyle = 'rgb(0,0,0)';
@@ -28,7 +29,29 @@ class Note {
                     ctx.stroke();
                 }
                 break;
-            case 4: // Hold Note
+
+            case 1: // Hold Note
+                if (t > 0) {
+                    ctx.lineWidth = 25 / t;
+                    ctx.strokeStyle = 'rgb(0,0,0)';
+                    ctx.beginPath();
+                    let f1 = to3D(startPos + railW * this.rail, h, t);
+                    let f2 = to3D(startPos + railW * (this.rail + 2), h, t);
+                    let f3 = to3D(startPos + railW * this.rail, h, t + this.detail);
+                    let f4 = to3D(startPos + railW * (this.rail + 2), h, t + this.detail);
+                    let hold = new Path2D();
+                    hold.moveTo(f1[0] + w / 2, f1[1]);
+                    hold.lineTo(f2[0] + w / 2, f2[1]);
+                    hold.lineTo(f4[0] + w / 2, f4[1]);
+                    hold.lineTo(f3[0] + w / 2, f3[1]);
+                    hold.lineTo(f1[0] + w / 2, f1[1]);
+                    ctx.fillStyle = 'rgb(0,0,0)';
+                    ctx.fill(hold, 'evenodd');
+                }
+                break;
+
+
+            case 4: // Change Note
                 if (time > this.time) {
                     poslane = this.rail;
                 }
@@ -37,21 +60,22 @@ class Note {
     }
 }
 
+notes.push(new Note(0, 1, 3, 1));   // Test chart creator
 for (let i = 0; i < 100; i++) {
-    notes.push(new Note(i % 2, 4, (60 / 140) * i));  // Test chart creator
+    notes.push(new Note(2, 0, (60 / 140) * i));
 }
 
-function to3D(x, y, z) {
+function to3D(x, y, z) {    // to3d(rail, canvas height, time)
     return [x / z, y / z];
 }
 
-function squareTo(pos, time) {
+function squareTo(pos, time) {      // smooth move rail area
     let startLane = poslane;
     let _startTime = Date.now();
     let interval = setInterval(() => {
         let elapsed = Date.now() - _startTime;
         let t = Math.min(elapsed / (time * 1000), 1);
-        poslane = startLane + (pos - startLane) * t; // 線性插值
+        poslane = startLane + (pos - startLane) * t;
         if (t === 1) clearInterval(interval);
     }, 16);
 }
@@ -107,7 +131,7 @@ function update() {
         note.drawNote();
     }
 
-    // Highlight rail position
+    // Highlight rail area
     ctx.strokeStyle = 'rgb(255, 0, 0)';
     ctx.lineWidth = 10;
     ctx.strokeRect(startPos + (w / 2) + railW * poslane, 0, h, h);
